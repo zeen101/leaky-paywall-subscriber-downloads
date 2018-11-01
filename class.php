200ab4ab27e54131da708c5d578a8dbaff0f522f
Jeremy Green <greenhornet79@gmail.com>
2018-11-01 08:41:40 -0600

    update logic determining if user can download file

--- class.php
+++ class.php
@@ -27,31 +27,49 @@ if ( ! class_exists( 'Leaky_Paywall_Subscriber_Downloads' ) ) {
 		}
 		
 		function process_requests() {
+
+			if ( !isset( $_GET['leaky-paywall-media-download'] ) ) {
+				return;
+			}
 								
 			global $leaky_paywall;
-			
 			$lp_settings = $leaky_paywall->get_settings();
+				
+			if ( $this->user_can_download_file() ) {
 			
-			if ( !empty( $_REQUEST['leaky-paywall-media-download'] ) ) {
+				leaky_paywall_mdo_server_download( absint( $_GET['leaky-paywall-media-download'] ) );
+			
+			} else {
 				
-				//Admins or subscribed users can download PDFs
-				if ( current_user_can( 'manage_options' ) || is_leaky_subscriber_logged_in() ) {
+				$output = '<h3>' . __( 'Unauthorized Download', 'lp-subscriber-downloads' ) . '</h3>';
+	
+				$output .= '<p>' . sprintf( __( 'You must be <a href="%s">logged in</a> with a valid <a href="%s">subscription</a> to download this file.', 'lp-subscriber-downloads' ), get_page_link( $lp_settings['page_for_login'] ), get_page_link( $lp_settings['page_for_subscription'] ) ) . '</p>';
+				$output .= '<a href="' . get_home_url() . '">' . sprintf( __( 'back to %s', 'lp-subscriber-downloads' ), $lp_settings['site_name'] ) . '</a>';
 				
-					leaky_paywall_mdo_server_download( $_REQUEST['leaky-paywall-media-download'] );
-				
-				} else {
-					
-					$output = '<h3>' . __( 'Unauthorize Download', 'lp-subscriber-downloads' ) . '</h3>';
-		
-					$output .= '<p>' . sprintf( __( 'You must be <a href="%s">logged in</a> with a valid subscription to download this file.', 'lp-subscriber-downloads' ), get_page_link( $lp_settings['page_for_login'] ) ) . '</p>';
-					$output .= '<a href="' . get_home_url() . '">' . sprintf( __( 'back to %s', 'lp-subscriber-downloads' ), $lp_settings['site_name'] ) . '</a>';
-					
-					wp_die( apply_filters( 'leaky_paywall_mdo_unauthorized_download_output', $output ) );
-					
-				}
+				wp_die( apply_filters( 'leaky_paywall_mdo_unauthorized_download_output', $output ) );
 				
 			}
 			
+		}
+
+		//Admins or subscribed users can download PDFs
+		protected function user_can_download_file() {
+
+			if ( !is_user_logged_in() ) {
+				return false;
+			}
+			
+			if ( current_user_can( 'manage_options' ) ) {
+				return true;
+			}
+
+		
+			if ( leaky_paywall_user_has_access() ) {
+				return true;
+			}
+			
+			return false;
+
 		}
 		
 	}
