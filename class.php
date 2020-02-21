41131e2a98ebdc0908fa0b8bbca0c37e6f75643d
Jeremy Green <greenhornet79@gmail.com>
2020-02-21 09:48:56 -0700

    update unautorized message based on users current level

--- class.php
+++ class.php
@@ -31,19 +31,30 @@ if ( ! class_exists( 'Leaky_Paywall_Subscriber_Downloads' ) ) {
 			if ( !isset( $_GET['leaky-paywall-media-download'] ) ) {
 				return;
 			}
+
+			$media_id = absint( $_GET['leaky-paywall-media-download'] );
 								
 			global $leaky_paywall;
 			$lp_settings = $leaky_paywall->get_settings();
 				
-			if ( $this->user_can_download_file() ) {
+			if ( $this->user_can_download_file( $media_id ) ) {
 			
-				leaky_paywall_mdo_server_download( absint( $_GET['leaky-paywall-media-download'] ) );
+				leaky_paywall_mdo_server_download( $media_id );
 			
 			} else {
-				
+
+				$lp_subscriber_downloads_specific_levels = get_post_meta( $media_id, '_lp_subscriber_downloads_specific_levels', true );
+
 				$output = '<h3>' . __( 'Unauthorized Download', 'lp-subscriber-downloads' ) . '</h3>';
-	
-				$output .= '<p>' . sprintf( __( 'You must be <a href="%s">logged in</a> with a valid <a href="%s">subscription</a> to download this file.', 'lp-subscriber-downloads' ), get_page_link( $lp_settings['page_for_login'] ), get_page_link( $lp_settings['page_for_subscription'] ) ) . '</p>';
+
+				if ( empty( $lp_subscriber_downloads_specific_levels ) && !is_user_logged_in() ) {
+					$output .= '<p>' . sprintf( __( 'You must be <a href="%s">logged in</a> with a valid <a href="%s">subscription</a> to download this file.', 'lp-subscriber-downloads' ), get_page_link( $lp_settings['page_for_login'] ), get_page_link( $lp_settings['page_for_subscription'] ) ) . '</p>';
+				} else if ( !empty( $lp_subscriber_downloads_specific_levels ) && is_user_logged_in() ) {
+					$output .= '<p>' . sprintf( __( 'You need a different <a href="%s">subscription</a> to download this file.', 'lp-subscriber-downloads' ), get_page_link( $lp_settings['page_for_subscription'] ) ) . '</p>';
+				} else {
+					$output .= '<p>' . sprintf( __( 'You must be <a href="%s">logged in</a> with a valid <a href="%s">subscription</a> to download this file.', 'lp-subscriber-downloads' ), get_page_link( $lp_settings['page_for_login'] ), get_page_link( $lp_settings['page_for_subscription'] ) ) . '</p>';
+				}
+				
 				$output .= '<a href="' . get_home_url() . '">' . sprintf( __( 'back to %s', 'lp-subscriber-downloads' ), $lp_settings['site_name'] ) . '</a>';
 				
 				wp_die( apply_filters( 'leaky_paywall_mdo_unauthorized_download_output', $output ) );
@@ -53,7 +64,7 @@ if ( ! class_exists( 'Leaky_Paywall_Subscriber_Downloads' ) ) {
 		}
 
 		//Admins or subscribed users can download PDFs
-		protected function user_can_download_file() {
+		protected function user_can_download_file( $media_id ) {
 
 			if ( !is_user_logged_in() ) {
 				return false;
@@ -62,8 +73,23 @@ if ( ! class_exists( 'Leaky_Paywall_Subscriber_Downloads' ) ) {
 			if ( current_user_can( 'manage_options' ) ) {
 				return true;
 			}
-		
-			if ( leaky_paywall_user_has_access( wp_get_current_user() ) ) {
+
+			$user = wp_get_current_user();
+
+			if ( !leaky_paywall_user_has_access( $user ) ) {
+				return false;
+			}
+
+			$lp_subscriber_downloads_specific_levels = get_post_meta( $media_id, '_lp_subscriber_downloads_specific_levels', true );
+
+			// if there are no specific levels set on the file, then any active subscriber gets access to it
+			if ( empty( $lp_subscriber_downloads_specific_levels ) ) {
+				return true;
+			}
+
+			$level_id = leaky_paywall_subscriber_current_level_id();
+
+			if ( in_array( $level_id, $lp_subscriber_downloads_specific_levels ) ) {
 				return true;
 			}
 			
